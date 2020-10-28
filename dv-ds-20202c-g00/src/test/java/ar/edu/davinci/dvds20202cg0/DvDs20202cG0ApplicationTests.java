556e4d42f068b2be88d6a6b9876c8d9738f4f8cf
frajomon <frajomon@gmail.com>
2020-10-28 20:38:14 -0300

    Commit inicial del proyecto final 20202C

---
+++ dv-ds-20202c-g00/src/test/java/ar/edu/davinci/dvds20202cg0/DvDs20202cG0ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
